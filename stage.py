1e737d34a5e388af4757d9473705fe07267f6acd
Jan Carlo Antinero <147511168+skyflakes02@users.noreply.github.com>
2024-06-23 03:44:09 +0800

    Update stage.py

--- stage.py
+++ stage.py
@@ -4,12 +4,13 @@ import random
 SCREEN_WIDTH = 415
 SCREEN_HEIGHT = 250
 TANK_SPEED = 2
+TANK_SPEED_2 = 3
 BULLET_SPEED = 4
 TANK_SPRITE_SIZE = 16
 CELL_SIZE = 16
 EXPLOSION_DURATION = 10
 
-UP, RIGHT, DOWN, LEFT = 0, 1, 2, 3
+UP, RIGHT, DOWN, LEFT, STOP = 0, 1, 2, 3, 4
 
 EMPTY, BRICK, STONE, WATER, SEMI_CRACKED_BRICK, CRACKED_BRICK, FOREST, HOME, MIRROR_NE, MIRROR_SE, POWER_UP = range(11)
 
@@ -77,7 +78,6 @@ class EnemyTank:
         self.shoot_interval = 60  
         self.shoot_timer = 0
         self.hits = 0
-        self.tank_type = 1
 
     def update(self, cells):
         if self.shoot_timer <= 0:
@@ -126,14 +126,78 @@ class EnemyTank:
         pyxel.play(0, 1)
 
     def draw(self):
-        if self.tank_type == 1:
-            tank_sprites = [(0, 48), (16, 48), (32, 48), (48, 48)]
-
-        elif self.tank_type == 2:
-            tank_sprites = [(0, 64), (16, 64), (32, 64), (48, 64)]
+        tank_sprites = [(0, 48), (16, 48), (32, 48), (48, 48)]
         pyxel.blt(self.x, self.y, 0, *tank_sprites[self.direction], TANK_SPRITE_SIZE, TANK_SPRITE_SIZE, 0) 
         for bullet in self.bullets:
             pyxel.rect(bullet.x, bullet.y, 2, 2, 8)
 
     def is_destroyed(self):
         return self.hits == 1
+    
+class EnemyTank2:
+    def __init__(self):
+        self.x = 0 * CELL_SIZE
+        self.y = 4 * CELL_SIZE
+        self.direction = RIGHT
+        self.bullets = []
+        self.shoot_interval = 60  
+        self.shoot_timer = 0
+        self.hits = 0
+
+    def update(self, cells):
+        if self.shoot_timer <= 0:
+            self.shoot_bullet()
+            self.shoot_timer = self.shoot_interval
+        else:
+            self.shoot_timer -= 0.3
+
+        if random.random() < 0.05: 
+            self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+
+        new_x = self.x
+        new_y = self.y
+
+        if self.direction == UP:
+            new_y -= TANK_SPEED_2
+        elif self.direction == DOWN:
+            new_y += TANK_SPEED_2
+        elif self.direction == LEFT:
+            new_x -= TANK_SPEED_2
+        elif self.direction == RIGHT:
+            new_x += TANK_SPEED_2
+
+        if not self.is_collision_with_cells(new_x, new_y, cells):
+            self.x = new_x
+            self.y = new_y
+
+        self.x = max(0, min(self.x, SCREEN_WIDTH - TANK_SPRITE_SIZE))
+        self.y = max(0, min(self.y, SCREEN_HEIGHT - TANK_SPRITE_SIZE))
+
+    def is_collision_with_cells(self, x, y, cells):
+        for cell in cells:
+            if cell.exists and cell.cell_type in {BRICK, SEMI_CRACKED_BRICK, CRACKED_BRICK, STONE, WATER, HOME, MIRROR_NE, MIRROR_SE}:
+                if (x < cell.x + CELL_SIZE and
+                    x + TANK_SPRITE_SIZE > cell.x and
+                    y < cell.y + CELL_SIZE and
+                    y + TANK_SPRITE_SIZE > cell.y):
+                    return True
+        return False
+
+    def shoot_bullet(self):
+        bullet_x = self.x + TANK_SPRITE_SIZE // 2 - 2
+        bullet_y = self.y + TANK_SPRITE_SIZE // 2 - 2
+        bullet = Bullet(bullet_x, bullet_y, self.direction, is_enemy=True)
+        self.bullets.append(bullet)
+        pyxel.play(0, 1)
+
+    def draw(self):
+        tank_sprites = [(0, 64), (16, 64), (32, 64), (48, 64)]
+        pyxel.blt(self.x, self.y, 0, *tank_sprites[self.direction], TANK_SPRITE_SIZE, TANK_SPRITE_SIZE, 0) 
+        for bullet in self.bullets:
+            pyxel.blt(bullet.x, bullet.y, 0, 32, 80, 16, 16, 0)
+
+    def is_destroyed(self):
+        return self.hits == 1
+    
+
+
